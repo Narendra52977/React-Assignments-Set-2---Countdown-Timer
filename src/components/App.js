105fef51595b1224176c274fe1b64dfd0cb66850
Narendra52977 <68866931+Narendra52977@users.noreply.github.com>
2020-12-02 19:42:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,19 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
+  const [time,setTime]=useState(0);
+  const handleTime=(event)=>{
+    let timeCount=Math.floor(event.target.value);
+    if(typeof(timeCount)==="number"&&timeCount>=0)setTime(timeCount);
+    else
+    setTime(0)
+  }
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={()=>handleTime(event)} value={time}/> sec.
         </h1>
       </div>
       <div id="current-time">{/* remaining time */}</div>
